1cbf9155f1d1f0c486d09e576e12cc07d4ab5f05
Flavio Scheck <flavio.scheck@gmail.com>
2022-01-26 21:57:10 -0300

    resolución fallas

--- pages/admin.py
+++ pages/admin.py
@@ -6,69 +6,65 @@ from import_export.admin import ImportExportModelAdmin
 class ArticleResource(resources.ModelResource):
     class Meta:
         model = Article
+class PageResource(resources.ModelResource):
+
+    class Meta: 
+        model = Page
+class AssideResource(resources.ModelResource):
+
+    class Meta:
+        model = Asside
+class VideoResource(resources.ModelResource):
+
+    class Meta:
+        model = Video
+class GalleryImagesResource(resources.ModelResource):
+    class Meta:
+        model = GalleryImages          
+class GalleryResource(resources.ModelResource):
+    class Meta:
+        model = Gallery  
+
+
 
 class ArticleAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
     list_display = ('title', 'public')
     resource_class = ArticleResource
 
-admin.site.register(Article, ArticleAdmin)
-
-class PageResource(resources.ModelResource):
-
-    class Meta: 
-        model = Page
-
 class PageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'title', 'slug', 'visible')
     resource_class = PageResource
 
-admin.site.register(Page, PageAdmin)
-
-class AssideResource(resources.ModelResource):
-
-    class Meta:
-        model = Asside
 class AssideAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'title')
     readonly_fields = ('created_at', 'updated_at')
     resources_class = AssideResource
 
-admin.site.register(Asside, AssideAdmin)
-
-class VideoResource(resources.ModelResource):
-
-    class Meta:
-        model = Video
-
 class VideoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'caption')
     resources_class = VideoResource
 
-admin.site.register(Video, VideoAdmin)
-
-# Gallery
-
-
-class GalleryImagesAdmin(admin.StackedInline):
-    model=GalleryImages
-
-class GalleryResource(resources.ModelResource):
-    class Meta:
-        model = Gallery  
-
 class GalleryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    inlines = [GalleryImagesAdmin]
+    
     resource_class = GalleryResource
-admin.site.register(Gallery, GalleryAdmin)
-
-class GalleryImagesResource(resources.ModelResource):
     class Meta:
-        model = GalleryImages  
-class GalleryImagesAdmin(admin.ModelAdmin):
-    pass
+        model=Gallery
+
+class GalleryImagesAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    
     resource_class = GalleryImagesResource
+
+
+# Registros
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Asside, AssideAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(GalleryImages, GalleryImagesAdmin)
+
 
 # Configurar el título del panel
 title = "ADS - Construcción en seco"
@@ -77,3 +73,5 @@ subtitle = "Panel de gestión"
 admin.site.site_header = title
 admin.site.site_title = title
 admin.site.index_title = subtitle
+
+
