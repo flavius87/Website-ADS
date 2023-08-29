c38c94c0fd48753fcfbd69267ad5dee1d312591e
Flavio Scheck <flavio.scheck@gmail.com>
2023-08-29 12:02:10 -0300

    vercel changes

--- adsweb/settings/production.py
+++ adsweb/settings/production.py
@@ -3,7 +3,7 @@ from decouple import config
 
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*', 'https://website-ads-production.up.railway.app']
+ALLOWED_HOSTS = ['.vercel.app']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
