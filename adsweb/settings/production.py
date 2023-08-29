d91dadd7eb0780e608330b5aa3b9bde635dbc6ac
Flavio Scheck <flavio.scheck@gmail.com>
2023-08-29 12:23:17 -0300

    a

--- adsweb/settings/production.py
+++ adsweb/settings/production.py
@@ -3,7 +3,7 @@ from decouple import config
 
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['website-ads.vercel.app']
+ALLOWED_HOSTS = ['*', 'https://website-ads-git-master-flavius87.vercel.app']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
