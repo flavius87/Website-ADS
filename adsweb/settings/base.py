a27800515d88554cc606673ce39cf0a8cdd2be49
Flavio Scheck <flavio.scheck@gmail.com>
2023-08-29 23:29:38 -0300

    update

--- adsweb/settings/base.py
+++ adsweb/settings/base.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
