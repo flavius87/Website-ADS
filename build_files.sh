9122b36028d7fb5406a2ac6a857ff6ba132d17c6
Flavio Scheck <flavio.scheck@gmail.com>
2023-09-06 14:41:10 -0300

    change build

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.11 manage.py collectstatic
+ echo "BUILD START"
+ python3.11 -m pip install -r requirements.txt
+ python3.11 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
