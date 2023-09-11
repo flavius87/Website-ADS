c09eac201727a7c2fd25123ff00f411978e387ef
Flavio Scheck <flavio.scheck@gmail.com>
2023-09-11 19:00:21 -0300

    ssds

--- adsweb/settings/production.py
+++ adsweb/settings/production.py
@@ -23,8 +23,8 @@ STATICFILES_DIRS = (BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER=config('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_USER='ads.construccionpampeana@gmail.com'
+EMAIL_HOST_PASSWORD='ads2022*'
 EMAIL_PORT=587
 
 # ckeditor
