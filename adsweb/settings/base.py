01609b8c0c7014ee74def88aa97d3a8574231810
Flavio Scheck <flavio.scheck@gmail.com>
2023-09-11 18:53:48 -0300

    mod

--- adsweb/settings/base.py
+++ adsweb/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', '!$77v*z&ekjh')
+SECRET_KEY = '!$77v*z&ekjh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
