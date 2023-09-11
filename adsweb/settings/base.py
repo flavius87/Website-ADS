792e292b7387ada2b6eb910d137cd14bcb34a6eb
Flavio Scheck <flavio.scheck@gmail.com>
2023-09-11 20:23:19 -0300

    static

--- adsweb/settings/base.py
+++ adsweb/settings/base.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
